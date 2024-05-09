806f1d1f30a691b59d544e070bdae526f70e4ce0
samaptikapanda <samaptikapanda>
2024-05-09 16:53:27 +0530

    Deployment Script changes by FDALERT user

---
+++ Migration/script.sql
@@ -0,0 +1,14 @@
+create or replace TABLE "DBO".ABCD (
+	EMPLOYEE_ID NUMBER(38,0),
+	FIRST_NAME VARCHAR(50),
+	LAST_NAME VARCHAR(50),
+	DEPARTMENT VARCHAR(100),
+	STATE VARCHAR(200)
+ "SANDBOX1".""reate or replace TABLE "DBO".ABCD (
+	EMPLOYEE_ID NUMBER(38,0),
+	FIRST_NAME VARCHAR(50),
+	LAST_NAME VARCHAR(50),
+	DEPARTMENT VARCHAR(100),
+	STATE VARCHAR(200)
+);
+-----------------------------------------------------------------------------
