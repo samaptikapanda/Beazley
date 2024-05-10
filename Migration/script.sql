294da6bcd8cd6fd894660b7cd4af19d0d5f10d54
samaptikapanda <samaptikapanda>
2024-05-10 11:08:46 +0530

    Deployment Script changes by FDALERT user

---
+++ Migration/script.sql
@@ -0,0 +1,10 @@
+create or replace TRANSIENT TABLE "DBO".STUDENT_TRANSIENT (
+	STUDENT_ID NUMBER(38,0),
+	STUDENT_NAME VARCHAR(50),
+	STUDENT_AGE NUMBER(38,0)
+ "SANDBOX1".""reate or replace TRANSIENT TABLE "DBO".STUDENT_TRANSIENT (
+	STUDENT_ID NUMBER(38,0),
+	STUDENT_NAME VARCHAR(50),
+	STUDENT_AGE NUMBER(38,0)
+);
+-----------------------------------------------------------------------------
