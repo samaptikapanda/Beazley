558dc6a56ab4534ab6601d770a5e8ee94d8b8cd9
Sam <79141193+samaptikapanda@users.noreply.github.com>
2024-05-10 10:40:15 +0530

    Delete SANDBOX directory

--- SANDBOX/DBO/TRANSIENT_TABLE/T_ORDERS.sql
+++
@@ -1,7 +0,0 @@
-create or replace TRANSIENT TABLE "DBO".T_ORDERS (
-	ORDER_ID NUMBER(38,0) NOT NULL,
-	PRODUCT_ID NUMBER(38,0),
-	QUANTITY NUMBER(38,0),
-	primary key (ORDER_ID),
-	foreign key (PRODUCT_ID) references DBO.T_PRODUCTS(PRODUCT_ID)
-);
