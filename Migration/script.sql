f618fd1fe0b39075c5902f20ba13371b0eed7826
samaptikapanda <samaptikapanda>
2024-05-12 13:06:11 +0530

    Deployment Script changes by FDALERT user

--- Migration/script.sql
+++ Migration/script.sql
@@ -1,5 +1,7 @@
-create or replace TABLE "DBO".TABLE1 (
-	ID NUMBER(38,0),
-	COL1 DATE
+create or replace TABLE "DBO"."UsersV3" (
+	ID NUMBER(38,0) autoincrement start 1 increment 1 noorder,
+	NAME VARCHAR(100) NOT NULL,
+	"Age" NUMBER(38,0),
+	"Age2" NUMBER(38,0)
 );
 -----------------------------------------------------------------------------
