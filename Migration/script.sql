89ee2db331129509302ae34ec613353bf4cfc60b
samaptika <samaptika.panda@performalytic.com>
2024-05-12 10:40:17 +0530

    test

--- Migration/script.sql
+++ Migration/script.sql
@@ -1,6 +1,10 @@
-create or replace TABLE "DBO".TABLE1 (
-	ID NUMBER(38,0),
-	COL1 DATE,
-	COL2 DATE
+create or replace TABLE "SANDBOX1"."DBO".ABCD (
+	EMPLOYEE_ID NUMBER(38,0),
+	FIRST_NAME VARCHAR(50),
+	LAST_NAME VARCHAR(50),
+	DEPARTMENT VARCHAR(100),
+	STATE VARCHAR(200)
 );
+-----------------------------------------------------------------------------
+
 -----------------------------------------------------------------------------
