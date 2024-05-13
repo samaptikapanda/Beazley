a20aae08b71daa33991dc948d4d73a301ecee49b
Sam <pandasamaptika96@gmail.com>
2024-05-13 16:54:35 +0530

    changes

--- actions/4DCHECKS/src/action.js
+++ actions/4DCHECKS/src/action.js
@@ -33,7 +33,7 @@ async function run() {
     Path: "DATABASE",
     HeadBranch: "develop",
     BaseBranch: "master",
-    ScriptGenerationRules : ["Include DROP statement"]
+    ScriptGenerationRules : [""]
   };
   const TokenFetchResponse = await fetch(
     `https://app.4dalert.com/api/v1/user-auth/login-user`,
