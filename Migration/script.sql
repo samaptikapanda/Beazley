14b87a0eedf6a118f66a741d7b1178b6e24a2975
Mangal Jena <55190271+Mangaljyotijena@users.noreply.github.com>
2024-05-13 10:43:36 +0530

    Update script.sql

--- Migration/script.sql
+++ Migration/script.sql
@@ -1,40 +1,12 @@
-create or replace TABLE "DBO".TEST_123 (
-	AA1 VARCHAR(30) NOT NULL DEFAULT '10avf',
-	BB1 BINARY(10),
-	CC22 NUMBER(38,0) NOT NULL autoincrement start 1 increment 1 order,
-	DD1 DATE,
-	primary key (AA1, CC22)
-);
------------------------------------------------------------------------------
+dvsdv sdv zdcsv
 
------------------------------------------------------------------------------
-ALTER TABLE "DBO".YES ADD ADDR11 VARCHAR(20);
------------------------------------------------------------------------------
-ALTER TABLE "DBO".YES ADD ADDR12 VARCHAR(20);
------------------------------------------------------------------------------
-ALTER TABLE "DBO".YES ADD ADDR13 VARCHAR(20);
------------------------------------------------------------------------------
-ALTER TABLE "DBO".YES ADD ADDR14 VARCHAR(20)
-;
------------------------------------------------------------------------------
 
------------------------------------------------------------------------------
-create or replace TABLE "DBO"."emp_trans1" (
-	NAME VARCHAR(50) NOT NULL,
-	"Emp_ID" NUMBER(38,0) NOT NULL,
-	primary key ("Emp_ID")
-);
------------------------------------------------------------------------------
 
------------------------------------------------------------------------------
-create or replace TABLE "DBO"."flight1" (
-	FLIGHTID NUMBER(38,0),
-	ID NUMBER(38,0),
-	FLIGHTNUMBER VARCHAR(80),
-	DEPARTUREDATE DATE,
-	DEPARTURECITY VARCHAR(50),
-	DESTINATIONCITY VARCHAR(50),
-	SEATNO NUMBER(38,0),
-	constraint PK primary key (ID, FLIGHTID)
-);
------------------------------------------------------------------------------
+
+
+
+
+
+
+
+vsdvc szdv xc
